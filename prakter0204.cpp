a52780930f983abd6d7e445a76c3167a478bf00d
fathurrahmanh <32325845+fathurrahmanh@users.noreply.github.com>
2017-10-24 07:50:39 -0700

    total biaya

--- prakter0204.cpp
+++ prakter0204.cpp
@@ -15,4 +15,7 @@ int main(){
 	int BPS=LR*20;
 	cout<<"biaya pasang (Rp)	: "<<BPS<<endl;
 	double TB=BPR+BPS;
+	cout<<"total biaya (Rp)		: "<<TB<<endl;
+	cout<<"------------------------------------";
+	getch();
 }
